d4b646c0aa993a0980e930a6683eb24452c3a2a4
Josue Martinho <josuem4rtinho@gmail.com>
2023-06-23 14:51:33 +0100

    adicionando e estilizando a api

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,10 +1,63 @@
-fetch("https://api.github.com/users/josuemartinho/repos")
-  .then((response) => response.json())
-  .then((data) => {
-    // Manipule os dados obtidos aqui
-    console.log(data);
-  })
-  .catch((error) => {
-    // Trate qualquer erro que ocorra durante a requisição
-    console.log(error);
-  });
+const repositoriesList = document.querySelector(".projetos-list");
+
+function getRepositories() {
+  fetch("https://api.github.com/users/josuemartinho/repos")
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((repo) => {
+        const listItem = document.createElement("li");
+        const projectLink = document.createElement("a");
+        const title = document.createElement("h2");
+        const languagesContainer = document.createElement("div"); // Container para as linguagens
+        const readme = document.createElement("p");
+        const buttonsDiv = document.createElement("div");
+        const repoLink = document.createElement("a");
+        const demoLink = document.createElement("a");
+
+        projectLink.href = repo.html_url;
+        title.textContent = repo.name;
+        readme.textContent = repo.description;
+        repoLink.textContent = "repositório";
+        repoLink.href = repo.html_url;
+        repoLink.classList.add("rep");
+        demoLink.textContent = "demonstração";
+        demoLink.href = repo.homepage;
+        demoLink.classList.add("dem");
+
+        projectLink.appendChild(title);
+        listItem.appendChild(projectLink);
+        listItem.appendChild(languagesContainer);
+        listItem.appendChild(readme);
+
+        buttonsDiv.classList.add("buttons");
+        buttonsDiv.appendChild(repoLink);
+        buttonsDiv.appendChild(demoLink);
+        listItem.appendChild(buttonsDiv);
+
+        repositoriesList.appendChild(listItem);
+
+        // Verificar se o repositório possui linguagens
+        if (repo.languages_url) {
+          fetch(repo.languages_url)
+            .then((response) => response.json())
+            .then((languages) => {
+              const languagesKeys = Object.keys(languages);
+              languagesKeys.forEach((language) => {
+                const languageSpan = document.createElement("span");
+                languageSpan.textContent = language;
+                languageSpan.classList.add("language");
+                languagesContainer.appendChild(languageSpan);
+              });
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+getRepositories();
